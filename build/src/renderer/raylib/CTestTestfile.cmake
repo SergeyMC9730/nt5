72bd4fdce106d66db4a627aed58b5e49d4963acb
SergeyMC9730 <bdsservers@yandex.ru>
2023-11-07 20:01:40 +0800

    Remove build directory

--- build/src/renderer/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mint/nt5/src/renderer/raylib
-# Build directory: /home/mint/nt5/build/src/renderer/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
