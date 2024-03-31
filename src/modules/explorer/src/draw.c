4d327fcb0fe72fe05746b478f6621ea8464488c2
SergeyMC9730 <bdsservers@yandex.ru>
2024-03-31 19:42:22 +0800

    ntmod_explorer: sidebar thing

--- src/modules/explorer/src/draw.c
+++ src/modules/explorer/src/draw.c
@@ -30,9 +30,102 @@
 
 #include <stdio.h>
 
+void explorer_draw_sidebar_box(Vector2 pos, const char *title, const char **elements) {
+    struct dwm_context *ctx = _ntDwmGetGlobal();
+    
+    // DrawRectangleRoundedLines()
+    renderer_state_t *st = _ntRendererGetState();
+    
+    int _bSzX = 185.f * st->scaling;
+    int _bSzY = 25.f * st->scaling;
+
+    pos.x += (1.f * st->scaling);
+    
+    Rectangle _b;
+    _b.x = pos.x;
+    _b.y = pos.y;
+    _b.width = _bSzX - (1.f * st->scaling);
+    _b.height = _bSzY;
+
+    Color gr1 = WHITE;
+    Color gr2 = WHITE;
+    gr2.r = 198;
+    gr2.g = 211;
+    gr2.b = 247;
+
+    // DrawRectangleRoundedLines(_b, 0.35f, 16.f, 1.f, WHITE);
+
+    // pos.x += 1.f * st->scaling;
+    // pos.y += 1.f * st->scaling;
+
+    int _tempY = 78.f * st->scaling;
+    
+    Color col2 = WHITE;
+    col2.r = 214;
+    col2.g = 223;
+    col2.b = 247;
+
+    DrawRectangle(pos.x, pos.y + _b.height, _b.width, _tempY - (1.f * st->scaling), col2);
+    DrawRectangleLines(pos.x, pos.y + _b.height - (1.f * st->scaling), _b.width, _tempY, WHITE);
+
+    DrawRectangle(pos.x, pos.y, _b.width - (98.f * st->scaling), _b.height, gr1);
+    
+    pos.x += 85.f * st->scaling;
+
+    DrawRectangleGradientH(pos.x, pos.y, _b.width - (85.f * st->scaling), _b.height, gr1, gr2);
+
+    if (!title) return;
+
+    // get bold tahoma font from the DWM
+    struct dwm_context_font font = _ntDwmGetFont(ctx, "tahomabd9");
+    
+    Vector2 titleSz = MeasureTextEx(font.font, title, font.real_size, font.spacing);
+
+    int titleY = (_bSzY - titleSz.y) / 2;
+    Color titleColor = {33, 93, 198, 255};
+
+    pos.x -= 85.f * st->scaling;
+
+    BeginScissorMode(pos.x, pos.y, _bSzX, _bSzY);
+    DrawTextEx(font.font, title, (Vector2){pos.x + (14.f * st->scaling), pos.y + titleY}, font.real_size, font.spacing, titleColor);
+    EndScissorMode();
+}
+
+void explorer_draw_sidebar1(struct dwm_window *wnd, void *user) {
+    struct dwm_context *ctx = _ntDwmGetGlobal();
+    struct local_module_state *lst = (struct local_module_state *)user;
+
+    Color _start = WHITE;
+    _start.r = 123;
+    _start.g = 162;
+    _start.b = 231;
+
+    Color _end = WHITE;
+    _end.r = 99;
+    _end.g = 117;
+    _end.b = 214;
+
+    renderer_state_t *st = _ntRendererGetState();
+
+    int offset = 1.f * st->scaling;
+    int xsz = 209.f * st->scaling;
+    int ysz = wnd->framebuffer.texture.height;
+
+    Rectangle r;
+    r.x = offset;
+    r.y = 0;
+    r.width = xsz;
+    r.height = ysz;
+
+    DrawRectangleGradientEx(r, _start, _end, _end, _end);
+
+    int cX = (xsz - 185.f * st->scaling) / 2;
+    explorer_draw_sidebar_box((Vector2){cX, 13.f * st->scaling}, "Test", NULL);
+}
+
 void explorer_draw(struct dwm_window *wnd, void *user) {
     struct dwm_context *ctx = _ntDwmGetGlobal();
-    // struct local_module_state *lst = (struct local_module_state *)user;
+    struct local_module_state *lst = (struct local_module_state *)user;
 
     // // printf("Explorer draw! (%d)\n", lst->fs->base.items_total);
 
@@ -57,17 +150,19 @@ void explorer_draw(struct dwm_window *wnd, void *user) {
 
     // _ntRendererDrawScreenPortion((Vector2){}, scr_pos, sz);
 
-    // // Color col = BLACK;
-    // // col.a = 220;
+    // Color col = BLACK;
+    // col.a = 220;
 
-    // // DrawRectangle(0, 0, sz.x, sz.y, col);
+    // DrawRectangle(0, 0, sz.x, sz.y, col);
 
     // Vector2 m = _ntDwmGetLocalMousePosition(ctx);
 
     // DrawRectangle(m.x, m.y, 8, 8, RED);
 
     // _ntTuiDrawMenu(lst->fs->base);
-    ClearBackground(ctx->theme.basic.background_color);
+    ClearBackground(WHITE);
+
+    explorer_draw_sidebar1(wnd, user);
 
     _ntRendererRunLuaScript("test.lua");
 }
