0fbf1db0549d8a76ea660e14cf10dd0cd4adf992
SergeyMC9730 <bdsservers@yandex.ru>
2024-08-20 01:10:17 +0000

    integrate cterm into the editor

--- src/animation_editor/src/main.c
+++ src/animation_editor/src/main.c
@@ -27,6 +27,8 @@
 #include <nt5emul/arrays/rsb_array_gen.h>
 #include <nt5emul/renderer_animation.h>
 #include <nt5emul/dwm/button.h>
+#include <cterm/cterm.h>
+#include <cterm/cterm_command_line.h>
 
 RSB_ARRAY_DEF_GEN(struct renderer_animation, RendererAnimation);
 RSB_ARRAY_IMPL_GEN(struct renderer_animation, RendererAnimation);
@@ -39,7 +41,8 @@ struct editor_object {
 
 void displayEditorObject(struct editor_object obj) {
     DrawRectangleRec(obj.rect, RAYWHITE);
-    DrawRectangleLines(obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height, WHITE);
+    //DrawRectangleLines(obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height, WHITE);
+    // DrawRectangleLinesEx(obj.rect, 2.f, WHITE);
 }
 
 RSB_ARRAY_DEF_GEN(struct editor_object, EditorObject);
@@ -69,6 +72,8 @@ struct editor_state {
     rsb_array_EditorObject *objects;
 
     struct dwm_context *dwm_ctx;
+
+    struct cterm_instance cterm_instance;
 };
 
 static struct editor_state __editor_state = {};
@@ -89,6 +94,17 @@ void DrawGrid2D(int sizeWidth, int sizeHeight, int spacing) {
     for (int i = -sizeWidth; i < sizeWidth; i += spacing) {
         DrawLine(i, -sizeHeight, i, sizeHeight, (Color){255, 255, 255, 30});
     }
+}
+
+Rectangle getVisibleCameraArea() {
+    int offscreen = 25;
+
+    Rectangle r = {__editor_state.cam.target.x - offscreen, __editor_state.cam.target.y - offscreen};
+
+    r.width = __editor_state.viewpoint.texture.width / __editor_state.cam.zoom + (offscreen * 2);
+    r.height = __editor_state.viewpoint.texture.height / __editor_state.cam.zoom + (offscreen * 2);
+
+    return r;
 }
 
 void drawViewpoint() {
@@ -211,11 +227,65 @@ void drawViewpoint() {
     ClearBackground((Color){30, 30, 30, 255});
 
     int limit = 100000 / 2;
+    int limit_y = limit / 20;
+
+    int spacing = 20;
+    float text_size = 8.f;
+    float text_spacing = 1.f;
+    int line_length = 5;
+
+    if (__editor_state.cam_zoom < 1.f) {
+        spacing = 40;
+        text_size = 10.f;
+    }
+    if (__editor_state.cam_zoom < 0.5f) {
+        spacing = 70;
+        text_size = 15.f;
+    }
+    if (__editor_state.cam_zoom >= 3.f) {
+        spacing = 10;
+        text_size = 4.f;
+    }
+    if (__editor_state.cam_zoom >= 9.f) {
+        spacing = 5;
+        text_size = 2.f;
+        text_spacing = 0.5f;
+        line_length = 2;
+    }
+
+    Rectangle view_area = getVisibleCameraArea();
+
+    char buf[64] = {0};
 
     DrawLine(-limit, 0, limit, 0, GRAY);
-    DrawLine(0, -limit, 0, limit, GRAY);
 
-    DrawGrid2D(limit, limit / 2, 20);
+    SetTextureFilter(GetFontDefault().texture, TEXTURE_FILTER_POINT);
+
+    for (int i = -limit; i < limit; i += spacing) {
+        if (CheckCollisionPointRec((Vector2){i, 0}, view_area)) {
+            DrawLine(i, line_length, i, -line_length, GRAY);
+
+            if (i != 0) {
+                snprintf(buf, 64, "%d", i);
+                DrawTextPro(GetFontDefault(), buf, (Vector2){i, line_length + 1}, (Vector2){}, 45.f, text_size, text_spacing, (Color){255, 255, 255, 64});
+            }
+        }
+    }
+
+    DrawLine(0, -limit_y, 0, limit_y, GRAY);
+
+    for (int i = -limit_y; i < limit_y; i += spacing) {
+        if (CheckCollisionPointRec((Vector2){0, i}, view_area)) {
+            DrawLine(-line_length, i, line_length, i, GRAY);
+
+            if (i != 0) {
+                snprintf(buf, 64, "%d", i);
+                DrawTextPro(GetFontDefault(), buf, (Vector2){line_length + 1, i}, (Vector2){}, 0.f, text_size, text_spacing, (Color){255, 255, 255, 64});
+            }
+        }
+    }
+
+    DrawGrid2D(limit, limit_y, 20);
 
     for (int i = 0; i < __editor_state.objects->len; i++) {
         struct editor_object obj = RSBGetAtIndexEditorObject(__editor_state.objects, i);
@@ -224,6 +294,7 @@ void drawViewpoint() {
     }
 
     EndMode2D();
+
     EndTextureModeStacked();
 }
 
@@ -262,10 +333,12 @@ void draw(void *ctx) {
 
     _ntTuiDrawFrame(rect, WHITE, buf);
 
+    SetTextureFilter(GetFontDefault().texture, TEXTURE_FILTER_BILINEAR);
+
     if (__editor_state.dwm_ctx != NULL) {
         {
             struct dwm_button btn = {};
-            btn.button = (Rectangle){gradientRect.x + 7, gradientRect.y + 15, 100, 25};
+            btn.button = (Rectangle){gradientRect.x + (7 * st->scaling) , gradientRect.y + (15* st->scaling), (100 * st->scaling), (25 * st->scaling)};
             btn.howered.ability = true;
             btn.activated.ability = true;
             btn.text = "New Object";
@@ -274,12 +347,23 @@ void draw(void *ctx) {
         
             if (state) {
                 printf("clicked 1\n");
+
+                Rectangle view_area = getVisibleCameraArea();
+
+                struct editor_object obj;
+                obj.animations = RSBCreateArrayRendererAnimation();
+                obj.rect = (Rectangle){0, 0, 10, 10};
+
+                RSBAddElementEditorObject(__editor_state.objects, obj);
+
+                struct cterm_execute_result r;
+		        _ctermExecute(&__editor_state.cterm_instance, "notify \"     Created new object", &r);
             }
         }
 
         {
             struct dwm_button btn = {};
-            btn.button = (Rectangle){gradientRect.x + 7, gradientRect.y + 45, 100, 25};
+            btn.button = (Rectangle){gradientRect.x + (7 * st->scaling), gradientRect.y + (45 * st->scaling), (100 * st->scaling), (25 * st->scaling)};
             btn.howered.ability = true;
             btn.activated.ability = true;
             btn.text = "Save";
@@ -327,6 +411,8 @@ void rendererThreadInit(void *ctx) {
     renderer_state_t *st = _ntRendererGetState();
     struct nt_tui_environment *st_tui = _ntGetTuiEnvironment();
 
+    // _ntRendererSetDpiScale(2.f);
+
     SetWindowMinSize(640 * st->scaling, 480 * st->scaling);
     SetWindowState(FLAG_WINDOW_RESIZABLE);
 
@@ -352,12 +438,11 @@ void rendererThreadInit(void *ctx) {
 
 void setupFontData(void *ctx) {
     struct dwm_context_font *font = _ntDwmGetFontRef(__editor_state.dwm_ctx, "tahomabd8");
+    renderer_state_t *st = _ntRendererGetState();
 
     if (font != NULL) {
-        font->real_size = 18;
+        font->real_size = 18 * st->scaling;
         font->spacing = 1;
-        
-        SetTextureFilter(font->font.texture, TEXTURE_FILTER_TRILINEAR);
     }
 }
 
@@ -382,6 +467,9 @@ int main(int argc, char **argv) {
     _ntDwmSetGlobal(__editor_state.dwm_ctx);
 
     _ntRendererPushQueue(setupFontData, NULL);
+
+    __editor_state.cterm_instance = _ctermInit(true);
+    __editor_state.cterm_instance.command_line.output = stdout;
 
     _ntRendererJoin();
 
